d3e460b6f1bf4e21f4ccaf28ffdd5abc0cf1022e
Hossein Salari <hossein.salari002@gmail.com>
2023-04-16 20:57:01 +0430

    create home section html css and js

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,56 @@
-let $ = document
+let $ = document;
 
-const navHamburger = $.querySelector(".nav-hamburger")
-const navList = $.querySelector(".nav-list")
-const navBg = $.querySelector(".nav-bg")
+const navHamburger = $.querySelector(".nav-hamburger");
+const navList = $.querySelector(".nav-list");
+const navBg = $.querySelector(".nav-bg");
+const slide = $.querySelector(".home-slide");
+const nextBtn = $.querySelector(".next-btn");
+const prevBtn = $.querySelector(".prev-btn");
 
+const bgImagesUrl = [
+  "../images/header-1.png",
+  "../images/header-2.png",
+  "../images/header-3.png",
+];
 
-let isOpenMenu = false
+let slideNumber = 1;
+let isOpenMenu = false;
 
 function hamburgerMenuHandler() {
-    isOpenMenu = !isOpenMenu
+  isOpenMenu = !isOpenMenu;
 
-    if(isOpenMenu) {
-        navList.style.transform = "translateX(0)"
-        navBg.style.width = "100%"
-        navHamburger.classList.add("transform")
-    } else {
-        navList.style.transform = "translateX(100%)"
-        navBg.style.width = "0"
-        navHamburger.classList.remove("transform")
-    }
+  if (isOpenMenu) {
+    navList.style.transform = "translateX(0)";
+    navBg.style.width = "100%";
+    navHamburger.classList.add("transform");
+  } else {
+    navList.style.transform = "translateX(100%)";
+    navBg.style.width = "0";
+    navHamburger.classList.remove("transform");
+  }
 }
 
-navHamburger.addEventListener("click", hamburgerMenuHandler)
+// change the home section bg image
+function nextHomeBackgroundHandler() {
+  slideNumber++;
+
+  if (slideNumber > bgImagesUrl.length) {
+    slideNumber = 1;
+  }
+
+  slide.style.background = `url(${bgImagesUrl[slideNumber - 1]})`;
+}
+
+function prevHomeBackgroundHandler() {
+  slideNumber--;
+
+  if (slideNumber === 0) {
+    slideNumber = bgImagesUrl.length;
+  }
+
+  slide.style.background = `url(${bgImagesUrl[slideNumber - 1]})`;
+}
+
+navHamburger.addEventListener("click", hamburgerMenuHandler);
+nextBtn.addEventListener("click", nextHomeBackgroundHandler);
+prevBtn.addEventListener("click", prevHomeBackgroundHandler);
